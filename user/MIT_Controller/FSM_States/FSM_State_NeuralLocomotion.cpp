d0892cf5f450533cc58c0896de6dce9b846b1a90
unknown <gmargo@mit.edu>
2020-05-25 13:17:49 -0400

    connect neural FSM to vision MPC

--- user/MIT_Controller/FSM_States/FSM_State_NeuralLocomotion.cpp
+++ user/MIT_Controller/FSM_States/FSM_State_NeuralLocomotion.cpp
@@ -1,4 +1,4 @@
-/*============================ Locomotion =============================*/
+/*============================ Vision =============================*/
 /**
  * FSM State for robot locomotion. Manages the contact specific logic
  * and handles calling the interfaces to the controllers. This state
@@ -8,7 +8,6 @@
 #include "FSM_State_NeuralLocomotion.h"
 #include <Utilities/Timer.h>
 #include <Controllers/WBC_Ctrl/LocomotionCtrl/LocomotionCtrl.hpp>
-//#include <rt/rt_interface_lcm.h>
 
 /**
  * Constructor for the FSM State that passes in state specific info to
@@ -17,26 +16,18 @@
  * @param _controlFSMData holds all of the relevant control data
  */
 template <typename T>
-FSM_State_NeuralLocomotion<T>::FSM_State_NeuralLocomotion(ControlFSMData<T>* _controlFSMData)
-    : FSM_State<T>(_controlFSMData, FSM_StateName::NEURAL_LOCOMOTION, "NEURAL_LOCOMOTION")
+FSM_State_NeuralLocomotion<T>::FSM_State_NeuralLocomotion(
+    ControlFSMData<T>* _controlFSMData)
+    : FSM_State<T>(_controlFSMData, FSM_StateName::NEURAL_LOCOMOTION, "NEURAL_LOCOMOTION"),
+        vision_MPC(_controlFSMData->controlParameters->controller_dt,
+                30 / (1000. * _controlFSMData->controlParameters->controller_dt),
+                _controlFSMData->userParameters),
+        cMPCOld(_controlFSMData->controlParameters->controller_dt,
+                30 / (1000. * _controlFSMData->controlParameters->controller_dt),
+                _controlFSMData->userParameters),
+         _visionLCM(getLcmUrl(255))
 {
-  if(_controlFSMData->_quadruped->_robotType == RobotType::MINI_CHEETAH){
-    cMPCOld = new ConvexMPCLocomotion(_controlFSMData->controlParameters->controller_dt,
-        //30 / (1000. * _controlFSMData->controlParameters->controller_dt),
-        //22 / (1000. * _controlFSMData->controlParameters->controller_dt),
-        27 / (1000. * _controlFSMData->controlParameters->controller_dt),
-        _controlFSMData->userParameters);
-
-  }else if(_controlFSMData->_quadruped->_robotType == RobotType::CHEETAH_3){
-    cMPCOld = new ConvexMPCLocomotion(_controlFSMData->controlParameters->controller_dt,
-        33 / (1000. * _controlFSMData->controlParameters->controller_dt),
-        _controlFSMData->userParameters);
-
-  }else{
-    assert(false);
-  }
-
-
+  // Set the safety checks
   this->turnOnAllSafetyChecks();
   // Turn off Foot pos command since it is set in WBC as operational task
   this->checkPDesFoot = false;
@@ -46,7 +37,60 @@ FSM_State_NeuralLocomotion<T>::FSM_State_NeuralLocomotion(ControlFSMData<T>* _co
   this->footstepLocations = Mat34<T>::Zero();
   _wbc_ctrl = new LocomotionCtrl<T>(_controlFSMData->_quadruped->buildModel());
   _wbc_data = new LocomotionCtrlData<T>();
+  zero_vec3.setZero();
+  _global_robot_loc.setZero();
+  _robot_rpy.setZero();
+
+  _visionLCM.subscribe("local_heightmap", &FSM_State_NeuralLocomotion<T>::handleHeightmapLCM, this);
+  _visionLCM.subscribe("traversability", &FSM_State_NeuralLocomotion<T>::handleIndexmapLCM, this);
+  _visionLCM.subscribe("global_to_robot", &FSM_State_NeuralLocomotion<T>::handleLocalization, this);
+  _visionLCMThread = std::thread(&FSM_State_NeuralLocomotion<T>::visionLCMThread, this);
+
+  _height_map = DMat<T>::Zero(x_size, y_size);
+  _idx_map = DMat<int>::Zero(x_size, y_size);
 }
+template<typename T>
+void FSM_State_NeuralLocomotion<T>::handleLocalization(const lcm::ReceiveBuffer* rbuf, 
+    const std::string& chan, const localization_lcmt* msg){
+  (void)rbuf;
+  (void)chan;
+
+  for(size_t i(0); i<3; ++i){
+    _robot_rpy[i] = msg->rpy[i];
+    _global_robot_loc[i] = msg->xyz[i];
+  }
+  _b_localization_data = true;
+}
+
+template<typename T>
+void FSM_State_NeuralLocomotion<T>::handleHeightmapLCM(const lcm::ReceiveBuffer *rbuf,
+                                      const std::string &chan,
+                                      const heightmap_t *msg) {
+  (void)rbuf;
+  (void)chan;
+
+  for(size_t i(0); i<x_size; ++i){
+    for(size_t j(0); j<y_size; ++j){
+      _height_map(i,j) = msg->map[i][j];
+    }
+  }
+}
+
+
+template<typename T>
+void FSM_State_NeuralLocomotion<T>::handleIndexmapLCM(const lcm::ReceiveBuffer *rbuf,
+    const std::string &chan,
+    const traversability_map_t *msg) {
+  (void)rbuf;
+  (void)chan;
+
+  for(size_t i(0); i<x_size; ++i){
+    for(size_t j(0); j<y_size; ++j){
+      _idx_map(i,j) = msg->map[i][j];
+    }
+  }
+}
+
 
 template <typename T>
 void FSM_State_NeuralLocomotion<T>::onEnter() {
@@ -55,9 +99,17 @@ void FSM_State_NeuralLocomotion<T>::onEnter() {
 
   // Reset the transition data
   this->transitionData.zero();
-  cMPCOld->initialize();
-  this->_data->_gaitScheduler->gaitData._nextGait = GaitType::TROT;
-  printf("[FSM LOCOMOTION] On Enter\n");
+  vision_MPC.initialize();
+
+  if(_b_localization_data){
+    _updateStateEstimator();
+    _ini_body_pos = _global_robot_loc;
+    _ini_body_ori_rpy = _robot_rpy;
+  }else{
+    _ini_body_pos = (this->_data->_stateEstimator->getResult()).position;
+    _ini_body_ori_rpy = (this->_data->_stateEstimator->getResult()).rpy;
+  }
+  printf("[FSM NEURAL LOCOMOTION] On Enter\n");
 }
 
 /**
@@ -65,13 +117,203 @@ void FSM_State_NeuralLocomotion<T>::onEnter() {
  */
 template <typename T>
 void FSM_State_NeuralLocomotion<T>::run() {
+  if(_b_localization_data){
+    _updateStateEstimator();
+  }
   // Call the locomotion control logic for this iteration
-  //printf("[FSM_State_NeuralLocomotion] Locomotion Control Step!");
-  LocomotionControlStep();
+  Vec3<T> des_vel; // x,y, yaw
+  _UpdateObstacle();
+
+  // Vision Walking
+  _UpdateVelCommand(des_vel);
+  _LocomotionControlStep(des_vel);
+
+  // Convex Locomotion
+  //_RCLocomotionControl();
+  
+
+  // Stand still
+  //_JPosStand(); 
+  _Visualization(des_vel);
 }
 
-extern rc_control_settings rc_control;
+template <typename T>
+void FSM_State_NeuralLocomotion<T>::_RCLocomotionControl() {
+  cMPCOld.run<T>(*this->_data);
 
+  if(this->_data->userParameters->use_wbc > 0.9){
+    _wbc_data->pBody_des = cMPCOld.pBody_des;
+    _wbc_data->vBody_des = cMPCOld.vBody_des;
+    _wbc_data->aBody_des = cMPCOld.aBody_des;
+
+    _wbc_data->pBody_RPY_des = cMPCOld.pBody_RPY_des;
+    _wbc_data->vBody_Ori_des = cMPCOld.vBody_Ori_des;
+    
+    for(size_t i(0); i<4; ++i){
+      _wbc_data->pFoot_des[i] = cMPCOld.pFoot_des[i];
+      _wbc_data->vFoot_des[i] = cMPCOld.vFoot_des[i];
+      _wbc_data->aFoot_des[i] = cMPCOld.aFoot_des[i];
+      _wbc_data->Fr_des[i] = cMPCOld.Fr_des[i]; 
+    }
+    _wbc_data->contact_state = cMPCOld.contact_state;
+
+    _wbc_ctrl->run(_wbc_data, *this->_data);
+
+  }
+}
+
+template<typename T>
+void FSM_State_NeuralLocomotion<T>::_updateStateEstimator(){
+  StateEstimate<T> * estimate_handle = this->_data->_stateEstimator->getResultHandle();
+
+  //printf("global robot:%f,%f,%f\n", _global_robot_loc[0], _global_robot_loc[1], _global_robot_loc[2]);
+  estimate_handle->position = _global_robot_loc;
+  estimate_handle->rpy = _robot_rpy;
+  // 
+  estimate_handle->rBody = rpyToRotMat(_robot_rpy);
+  estimate_handle->orientation = rpyToQuat(_robot_rpy);
+}
+
+template<typename T>
+void FSM_State_NeuralLocomotion<T>::_JPosStand(){
+  Vec3<T> stand_jpos;
+  stand_jpos << 0.f, -.8f, 1.6f;
+  for(int leg(0); leg<4;++leg){
+    this->jointPDControl(leg, stand_jpos, zero_vec3);
+  }
+}
+template <typename T>
+void FSM_State_NeuralLocomotion<T>::_UpdateObstacle(){
+  _obs_list.clear();
+  // TEST
+  //Vec3<T> test1; 
+  //test1<< 0.7, 0.20, 0.4;
+  //_obs_list.push_back(test1);
+  //test1<< 0.7, -0.20, 0.4; //_obs_list.push_back(test1); 
+  T obstacle_height = 0.15; 
+  T threshold_gap = 0.08; 
+  bool add_obs(true); 
+  Vec3<T> robot_loc; 
+  if(_b_localization_data){ robot_loc = _global_robot_loc; } 
+  else{ robot_loc = (this->_data->_stateEstimator->getResult()).position; } 
+  Vec3<T> obs; obs[2] = 0.27; 
+
+  T dist = 0;
+  for(size_t i(0); i<x_size; ++i){
+    for(size_t j(0); j<y_size; ++j){
+      if( (_height_map(i, j) > obstacle_height) ){ // if too high point
+        add_obs = true;
+        obs[0] = i*grid_size - 50*grid_size + robot_loc[0];
+        obs[1] = j*grid_size - 50*grid_size + robot_loc[1];
+
+        // check with already added obstacle
+        for(size_t idx_obs(0); idx_obs < _obs_list.size(); ++idx_obs){
+          dist = sqrt(  (obs[0] - _obs_list[idx_obs][0])*(obs[0] - _obs_list[idx_obs][0]) + 
+              (obs[1] - _obs_list[idx_obs][1])*(obs[1] - _obs_list[idx_obs][1]) ); 
+          if(dist < threshold_gap){
+            add_obs = false;
+          }
+        }// distance check
+        if(add_obs){
+            _obs_list.push_back(obs);
+        }
+      }
+    } // y loop
+  } // x loop
+
+  //_print_obstacle_list();
+}
+
+template <typename T>
+void FSM_State_NeuralLocomotion<T>::_print_obstacle_list(){
+  for(size_t i(0); i < _obs_list.size(); ++i){
+    printf("%lu th obstacle: %f, %f, %f\n", i, _obs_list[i][0], _obs_list[i][1], _obs_list[i][2] );
+  }
+}
+
+template <typename T>
+void FSM_State_NeuralLocomotion<T>::_Visualization(const Vec3<T> & des_vel){
+  velocity_visual_t vel_visual;
+  for(size_t i(0); i<3; ++i){
+    vel_visual.vel_cmd[i] = des_vel[i];
+    vel_visual.base_position[i] = (this->_data->_stateEstimator->getResult()).position[i];
+  }
+  _visionLCM.publish("velocity_cmd", &vel_visual);
+  
+
+  _obs_visual_lcm.num_obs = _obs_list.size();
+  for(size_t i(0); i<_obs_list.size(); ++i){
+    _obs_visual_lcm.location[i][0] = _obs_list[i][0];
+    _obs_visual_lcm.location[i][1] = _obs_list[i][1];
+    _obs_visual_lcm.location[i][2] = _obs_list[i][2];
+  }
+  _obs_visual_lcm.sigma = 0.15;
+  _obs_visual_lcm.height = 0.5;
+
+  _visionLCM.publish("obstacle_visual", &_obs_visual_lcm);
+}
+
+template <typename T>
+void FSM_State_NeuralLocomotion<T>::_UpdateVelCommand(Vec3<T> & des_vel) {
+  des_vel.setZero();
+
+  Vec3<T> target_pos, curr_pos, curr_ori_rpy;
+
+  target_pos.setZero();
+
+  T moving_time = 25.0;
+  T curr_time = (T)iter * 0.002;
+  //if(curr_time > moving_time){
+    //curr_time = moving_time;
+  //}
+  //target_pos[0] += 2.0 * curr_time/moving_time;
+  //target_pos[0] += 2.5 * curr_time/moving_time;
+  //target_pos[0] = 0.7 * (1-cos(2*M_PI*curr_time/moving_time));
+  target_pos[0] = 1.0 * (1-cos(2*M_PI*curr_time/moving_time));
+
+  if(_b_localization_data){
+    curr_pos = _global_robot_loc;
+    curr_pos -= _ini_body_pos;
+    curr_ori_rpy = _robot_rpy;
+  }else{
+    curr_pos = (this->_data->_stateEstimator->getResult()).position;
+    curr_pos -= _ini_body_pos;
+    curr_ori_rpy = (this->_data->_stateEstimator->getResult()).rpy;
+
+  }
+  target_pos = rpyToRotMat(_ini_body_ori_rpy).transpose() * target_pos;
+  des_vel[0] = 0.7 * (target_pos[0] - curr_pos[0]);
+  des_vel[1] = 0.7 * (target_pos[1] - curr_pos[1]);
+  des_vel[2] = 0.7 * (_ini_body_ori_rpy[2] - curr_ori_rpy[2]);
+
+  //des_vel[0] = 0.5;
+  T inerproduct;
+  T x, y, x_obs, y_obs;
+  T vel_x, vel_y;
+  T sigma(0.15);
+  T h(0.5);
+  for(size_t i(0); i<_obs_list.size(); ++i){
+    x = curr_pos[0];
+    y = curr_pos[1];
+    x_obs = _obs_list[i][0];
+    y_obs = _obs_list[i][1];
+    h = _obs_list[i][2];
+
+    inerproduct = (x-x_obs)*(x-x_obs) + (y-y_obs)*(y-y_obs);
+    vel_x = h*((x-x_obs)/ sigma/sigma)*exp(-inerproduct/(2*sigma*sigma));
+    vel_y = h*((y-y_obs)/ sigma/sigma)*exp(-inerproduct/(2*sigma*sigma));
+    //printf("vel_x, y: %f, %f\n", vel_x, vel_y);
+
+    des_vel[0] += vel_x;
+    des_vel[1] += vel_y;
+  }
+  //printf("des vel_x, y: %f, %f\n", des_vel[0], des_vel[1]);
+
+  des_vel[0] = fminf(fmaxf(des_vel[0], -1.), 1.);
+  des_vel[1] = fminf(fmaxf(des_vel[1], -1.), 1.);
+
+}
+ 
 /**
  * Manages which states can be transitioned into either by the user
  * commands or state event triggers.
@@ -84,55 +326,43 @@ FSM_StateName FSM_State_NeuralLocomotion<T>::checkTransition() {
   iter++;
 
   // Switch FSM control mode
-  if(locomotionSafe()) {
-    switch ((int)this->_data->controlParameters->control_mode) {
-      case K_NEURAL_LOCOMOTION:
-        break;
+  switch ((int)this->_data->controlParameters->control_mode) {
+    case K_NEURAL_LOCOMOTION:
+      break;
 
-      case K_BALANCE_STAND:
-        // Requested change to BALANCE_STAND
-        this->nextStateName = FSM_StateName::BALANCE_STAND;
+    case K_LOCOMOTION:
+      this->nextStateName = FSM_StateName::LOCOMOTION;
+      this->transitionDuration = 0.0;
+      break;
 
-        // Transition time is immediate
-        this->transitionDuration = 0.0;
+    case K_BALANCE_STAND:
+      // Requested change to BALANCE_STAND
+      this->nextStateName = FSM_StateName::BALANCE_STAND;
 
-        break;
+      // Transition time is immediate
+      this->transitionDuration = 0.0;
 
-      case K_PASSIVE:
-        // Requested change to BALANCE_STAND
-        this->nextStateName = FSM_StateName::PASSIVE;
+      break;
 
-        // Transition time is immediate
-        this->transitionDuration = 0.0;
+    case K_PASSIVE:
+      // Requested change to BALANCE_STAND
+      this->nextStateName = FSM_StateName::PASSIVE;
 
-        break;
+      // Transition time is immediate
+      this->transitionDuration = 0.0;
 
-      case K_STAND_UP:
-        this->nextStateName = FSM_StateName::STAND_UP;
-        this->transitionDuration = 0.;
-        break;
+      break;
 
-      case K_RECOVERY_STAND:
-        this->nextStateName = FSM_StateName::RECOVERY_STAND;
-        this->transitionDuration = 0.;
-        break;
+    case K_RECOVERY_STAND:
+      this->nextStateName = FSM_StateName::RECOVERY_STAND;
+      this->transitionDuration = 0.;
+      break;
 
-      case K_VISION:
-        this->nextStateName = FSM_StateName::VISION;
-        this->transitionDuration = 0.;
-        break;
-
-      default:
-        std::cout << "[CONTROL FSM] Bad Request: Cannot transition from "
-                  << K_LOCOMOTION << " to "
-                  << this->_data->controlParameters->control_mode << std::endl;
-    }
-  } else {
-    this->nextStateName = FSM_StateName::RECOVERY_STAND;
-    this->transitionDuration = 0.;
-    rc_control.mode = RC_mode::RECOVERY_STAND;
+    default:
+      std::cout << "[CONTROL FSM] Bad Request: Cannot transition from "
+                << K_NEURAL_LOCOMOTION << " to "
+                << this->_data->controlParameters->control_mode << std::endl;
   }
-
 
   // Return the next state name to the FSM
   return this->nextStateName;
@@ -149,7 +379,7 @@ TransitionData<T> FSM_State_NeuralLocomotion<T>::transition() {
   // Switch FSM control mode
   switch (this->nextStateName) {
     case FSM_StateName::BALANCE_STAND:
-      LocomotionControlStep();
+      _LocomotionControlStep(zero_vec3);
 
       iter++;
       if (iter >= this->transitionDuration * 1000) {
@@ -162,20 +392,15 @@ TransitionData<T> FSM_State_NeuralLocomotion<T>::transition() {
 
     case FSM_StateName::PASSIVE:
       this->turnOffAllSafetyChecks();
-
       this->transitionData.done = true;
 
-      break;
-
-    case FSM_StateName::STAND_UP:
-      this->transitionData.done = true;
       break;
 
     case FSM_StateName::RECOVERY_STAND:
       this->transitionData.done = true;
       break;
 
-    case FSM_StateName::VISION:
+    case FSM_StateName::LOCOMOTION:
       this->transitionData.done = true;
       break;
 
@@ -187,46 +412,6 @@ TransitionData<T> FSM_State_NeuralLocomotion<T>::transition() {
 
   // Return the transition data to the FSM
   return this->transitionData;
-}
-
-template<typename T>
-bool FSM_State_NeuralLocomotion<T>::locomotionSafe() {
-  auto& seResult = this->_data->_stateEstimator->getResult();
-
-  const T max_roll = 40;
-  const T max_pitch = 40;
-
-  if(std::fabs(seResult.rpy[0]) > ori::deg2rad(max_roll)) {
-    printf("Unsafe locomotion: roll is %.3f degrees (max %.3f)\n", ori::rad2deg(seResult.rpy[0]), max_roll);
-    return false;
-  }
-
-  if(std::fabs(seResult.rpy[1]) > ori::deg2rad(max_pitch)) {
-    printf("Unsafe locomotion: pitch is %.3f degrees (max %.3f)\n", ori::rad2deg(seResult.rpy[1]), max_pitch);
-    return false;
-  }
-
-  for(int leg = 0; leg < 4; leg++) {
-    auto p_leg = this->_data->_legController->datas[leg].p;
-    if(p_leg[2] > 0) {
-      printf("Unsafe locomotion: leg %d is above hip (%.3f m)\n", leg, p_leg[2]);
-      return false;
-    }
-
-    if(std::fabs(p_leg[1] > 0.18)) {
-      printf("Unsafe locomotion: leg %d's y-position is bad (%.3f m)\n", leg, p_leg[1]);
-      return false;
-    }
-
-    auto v_leg = this->_data->_legController->datas[leg].v.norm();
-    if(std::fabs(v_leg) > 9.) {
-      printf("Unsafe locomotion: leg %d is moving too quickly (%.3f m/s)\n", leg, v_leg);
-      return false;
-    }
-  }
-
-  return true;
-
 }
 
 /**
@@ -244,63 +429,30 @@ void FSM_State_NeuralLocomotion<T>::onExit() {
  * each stance or swing leg.
  */
 template <typename T>
-void FSM_State_NeuralLocomotion<T>::LocomotionControlStep() {
+void FSM_State_NeuralLocomotion<T>::_LocomotionControlStep(const Vec3<T> & des_vel) {
   // StateEstimate<T> stateEstimate = this->_data->_stateEstimator->getResult();
 
   // Contact state logic
-  // estimateContact();
-
-  cMPCOld->run<T>(*this->_data);
-  Vec3<T> pDes_backup[4];
-  Vec3<T> vDes_backup[4];
-  Mat3<T> Kp_backup[4];
-  Mat3<T> Kd_backup[4];
-
-  for(int leg(0); leg<4; ++leg){
-    pDes_backup[leg] = this->_data->_legController->commands[leg].pDes;
-    vDes_backup[leg] = this->_data->_legController->commands[leg].vDes;
-    Kp_backup[leg] = this->_data->_legController->commands[leg].kpCartesian;
-    Kd_backup[leg] = this->_data->_legController->commands[leg].kdCartesian;
-  }
+  vision_MPC.run<T>(*this->_data, des_vel, _height_map, _idx_map);
 
   if(this->_data->userParameters->use_wbc > 0.9){
-    _wbc_data->pBody_des = cMPCOld->pBody_des;
-    _wbc_data->vBody_des = cMPCOld->vBody_des;
-    _wbc_data->aBody_des = cMPCOld->aBody_des;
+    _wbc_data->pBody_des = vision_MPC.pBody_des;
+    _wbc_data->vBody_des = vision_MPC.vBody_des;
+    _wbc_data->aBody_des = vision_MPC.aBody_des;
 
-    _wbc_data->pBody_RPY_des = cMPCOld->pBody_RPY_des;
-    _wbc_data->vBody_Ori_des = cMPCOld->vBody_Ori_des;
+    _wbc_data->pBody_RPY_des = vision_MPC.pBody_RPY_des;
+    _wbc_data->vBody_Ori_des = vision_MPC.vBody_Ori_des;
     
     for(size_t i(0); i<4; ++i){
-      _wbc_data->pFoot_des[i] = cMPCOld->pFoot_des[i];
-      _wbc_data->vFoot_des[i] = cMPCOld->vFoot_des[i];
-      _wbc_data->aFoot_des[i] = cMPCOld->aFoot_des[i];
-      _wbc_data->Fr_des[i] = cMPCOld->Fr_des[i]; 
+      _wbc_data->pFoot_des[i] = vision_MPC.pFoot_des[i];
+      _wbc_data->vFoot_des[i] = vision_MPC.vFoot_des[i];
+      _wbc_data->aFoot_des[i] = vision_MPC.aFoot_des[i];
+      _wbc_data->Fr_des[i] = vision_MPC.Fr_des[i]; 
     }
-    _wbc_data->contact_state = cMPCOld->contact_state;
+    _wbc_data->contact_state = vision_MPC.contact_state;
     _wbc_ctrl->run(_wbc_data, *this->_data);
-  }
-  for(int leg(0); leg<4; ++leg){
-    //this->_data->_legController->commands[leg].pDes = pDes_backup[leg];
-    this->_data->_legController->commands[leg].vDes = vDes_backup[leg];
-    //this->_data->_legController->commands[leg].kpCartesian = Kp_backup[leg];
-    this->_data->_legController->commands[leg].kdCartesian = Kd_backup[leg];
-  }
 
+  }
 }
 
-/**
- * Stance leg logic for impedance control. Prevent leg slipping and
- * bouncing, as well as tracking the foot velocity during high speeds.
- */
-template <typename T>
-void FSM_State_NeuralLocomotion<T>::StanceLegImpedanceControl(int leg) {
-  // Impedance control for the stance leg
-  this->cartesianImpedanceControl(
-      leg, this->footstepLocations.col(leg), Vec3<T>::Zero(),
-      this->_data->controlParameters->stand_kp_cartesian,
-      this->_data->controlParameters->stand_kd_cartesian);
-}
-
-// template class FSM_State_NeuralLocomotion<double>;
 template class FSM_State_NeuralLocomotion<float>;
--- user/MIT_Controller/FSM_States/FSM_State_NeuralLocomotion.h
+++ user/MIT_Controller/FSM_States/FSM_State_NeuralLocomotion.h
@@ -2,7 +2,15 @@
 #define FSM_STATE_NEURALLOCOMOTION_H
 
 #include <Controllers/convexMPC/ConvexMPCLocomotion.h>
+#include <Controllers/VisionMPC/VisionMPCLocomotion.h>
 #include "FSM_State.h"
+#include <thread>
+#include <lcm-cpp.hpp>
+#include "heightmap_t.hpp"
+#include "traversability_map_t.hpp"
+#include "velocity_visual_t.hpp"
+#include "obstacle_visual_t.hpp"
+#include "localization_lcmt.hpp"
 
 template<typename T> class WBC_Ctrl;
 template<typename T> class LocomotionCtrlData;
@@ -29,21 +37,52 @@ class FSM_State_NeuralLocomotion : public FSM_State<T> {
 
   // Behavior to be carried out when exiting a state
   void onExit();
-
+  
  private:
   // Keep track of the control iterations
   int iter = 0;
-  ConvexMPCLocomotion* cMPCOld;
+  VisionMPCLocomotion vision_MPC;
+  ConvexMPCLocomotion cMPCOld;
+
   WBC_Ctrl<T> * _wbc_ctrl;
   LocomotionCtrlData<T> * _wbc_data;
 
-  // Parses contact specific controls to the leg controller
-  void LocomotionControlStep();
+  Vec3<T> _ini_body_pos;
+  Vec3<T> _ini_body_ori_rpy;
+  Vec3<T> zero_vec3;
+  Vec3<T> _global_robot_loc;
+  Vec3<T> _robot_rpy;
 
-  bool locomotionSafe();
+  size_t x_size = 100;
+  size_t y_size = 100;
+  double grid_size = 0.015;
 
-  // Impedance control for the stance legs during locomotion
-  void StanceLegImpedanceControl(int leg);
+  DMat<T> _height_map;
+  DMat<int> _idx_map;
+
+  void handleHeightmapLCM(const lcm::ReceiveBuffer* rbuf, const std::string& chan, const heightmap_t* msg);
+  void handleIndexmapLCM(const lcm::ReceiveBuffer* rbuf, const std::string& chan, const traversability_map_t* msg);
+  void handleLocalization(const lcm::ReceiveBuffer* rbuf, const std::string& chan, const localization_lcmt* msg);
+  bool _b_localization_data = false;
+  void visionLCMThread() { while (true) { _visionLCM.handle(); } }
+
+  lcm::LCM _visionLCM;
+  std::thread _visionLCMThread;
+
+  vectorAligned< Vec3<T> > _obs_list; // loc, height
+  obstacle_visual_t _obs_visual_lcm;
+
+  void _updateStateEstimator();
+  void _JPosStand();
+  void _UpdateObstacle();
+  void _LocomotionControlStep(const Vec3<T> & vel_cmd);
+  void _UpdateVelCommand(Vec3<T> & vel_cmd);
+  void _RCLocomotionControl();
+  void _Visualization(const Vec3<T> & des_vel);
+  void _print_obstacle_list();
+
+  Vec3<T> _target_pos;
+
 };
 
-#endif  // FSM_State_NeuralLocomotion_H
+#endif  // FSM_STATE_LOCOMOTION_H

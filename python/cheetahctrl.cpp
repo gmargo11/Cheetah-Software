d7fe087d12bc80fab8dc7718a69abe0f56884865
unknown <gmargo@mit.edu>
2020-06-03 11:00:05 -0400

    add python bindings for WBC

---
+++ python/cheetahctrl.cpp
@@ -0,0 +1,23 @@
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+#include <Controllers/WBC_Ctrl/LocomotionCtrl/LocomotionCtrl.hpp>
+#include <Dynamics/FloatingBaseModel.h>
+#include <FSM_States/ControlFSMData.h>
+
+
+PYBIND11_MODULE(cheetahctrl, m) {
+    py::class_<LocomotionCtrl>(m, "LocomotionCtrl")
+        .def(py::init<FloatingBaseModel>())
+        .def("run", &LocomotionCtrl::run)
+
+    py::class_<FloatingBaseModel>(m, "FloatingBaseModel")
+        .def(py::init<>())
+        .def("addBase", &FloatingBaseModel::addBase)
+        .def("addBody", &FloatingBaseModel::addBody)
+        .def("addGroundContactPoint", &FloatingBaseModel::addGroundContactPoint)
+        //.def("setState", &FloatingBaseModel::addBase)
+
+    
+}
+

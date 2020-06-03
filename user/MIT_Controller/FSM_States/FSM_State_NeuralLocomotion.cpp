a541dd2cd2a5a0c948fd70f70146f899088332f5
unknown <gmargo@mit.edu>
2020-06-02 23:01:12 -0400

    fix variable names

--- user/MIT_Controller/FSM_States/FSM_State_NeuralLocomotion.cpp
+++ user/MIT_Controller/FSM_States/FSM_State_NeuralLocomotion.cpp
@@ -127,9 +127,10 @@ void FSM_State_NeuralLocomotion<T>::handleGaitTargetLCM(const lcm::ReceiveBuffer
   target_vel[0] = msg->vel[0];
   target_vel[1] = msg->vel[1];
   //target_p_foot = msg->p_foot;
-  for(int i=0; i<4; i++){}
-    offset_cmd = msg->offsets[i];
+  for(int i=0; i<4; i++){
+    offset_cmd[i] = msg->offsets[i];
     durations_cmd[i] = msg->durations[i];
+  }
 
   printf("Recieved gait target");
 

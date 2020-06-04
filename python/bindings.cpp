b216602289c52af100bb324cf89cc0261862bfe1
Gabriel Margolis <gmargo@mit.edu>
2020-06-04 06:51:42 +0000

    working binding

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -8,6 +8,7 @@ namespace py = pybind11;
 #include "Dynamics/FloatingBaseModel.h"
 #include "Dynamics/MiniCheetah.h"
 #include "Dynamics/Quadruped.h"
+#include "Dynamics/SpatialInertia.h"
 //#include "Graphics3D.h"
 //#include "SimControlPanel.h"
 //#include "SimulationRunnerHeadless.h"
@@ -18,8 +19,9 @@ namespace py = pybind11;
 PYBIND11_MODULE(pycheetah, m) {
 
 	py::class_<FloatingBaseModel<float>>(m, "FloatingBaseModel")
-		.def(py::init<>());
-		//.def("addBase", &FloatingBaseModel<float>::addBase)
+		.def(py::init<>())
+		.def("addBase", py::overload_cast<float,  const Vec3<float>&, const Mat3<float>&>(&FloatingBaseModel<float>::addBase), "", py::arg("mass"), py::arg("com"), py::arg("I"));
+		//.def("addBase", &FloatingBaseModel<float>::addBase(mass, const Vec3<T>& com, const Mat3<T>& I));//float, Vec3<float>&, Mat3<float>&));
 		//.def("addBody", &FloatingBaseModel<float>::addBody)
 		//.def("addGroundContactPoint", &FloatingBaseModel<float>::addGroundContactPoint);
 		//.def("setState", &FloatingBaseModel::addBase)

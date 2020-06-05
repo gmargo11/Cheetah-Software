ae9d9dc352f99dea89d003727209f234eece2627
Gabriel Margolis <gmargo@mit.edu>
2020-06-05 04:03:58 +0000

    add bindings for WBC

--- python/binding_tests.py
+++ python/binding_tests.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import pycheetah
-from pycheetah import FloatingBaseModel, ControlFSMData, LocomotionCtrl, LocomotionCtrlData, PeriodicTaskManager, RobotRunner, MIT_Controller, buildMiniCheetah
+from pycheetah import * #FloatingBaseModel, ControlFSMData, LocomotionCtrl, LocomotionCtrlData, PeriodicTaskManager, RobotRunner, MIT_Controller
 
 #fb = FloatingBaseModel()
 #fb.addGroundContactPoint(0, np.array([1., 1., 1.]), False)
@@ -12,23 +12,60 @@ cheetah = buildMiniCheetah()
 model = cheetah.buildModel()
 
 
+# test WBC
+
 lc = LocomotionCtrl(model)
 lc_data = LocomotionCtrlData()
-print(lc_data.vBody_des)
+#print(lc_data.vBody_des)i
+pBody_des = np.array([0.0, 0.0, 0.29])
+vBody_des, aBody_des, pBody_RPY_des, vBody_Ori_des = np.zeros(3), np.zeros(3), np.zeros(3) , np.zeros(3) 
 
-fsm_data = ControlFSMData()
-print(fsm_data)
+pFoot_des = np.array([[0.2, -0.15, 0.], [0.2, 0.15, 0.], [-0.2, -0.15, 0.], [-0.2, 0.15, 0.]])
+vFoot_des, aFoot_des, Fr_des = np.zeros((4, 3)), np.zeros((4, 3)), np.zeros((4, 3))
+contact_state = np.array([1, 1, 1, 1])
 
-lc.run(lc_data, fsm_data)
+lc_data.setBodyDes(pBody_des, vBody_des, aBody_des, pBody_RPY_des, vBody_Ori_des)
+for i in range(4):
+    lc_data.setFootDes(i, pFoot_des[i], vFoot_des[i], aFoot_des[i], Fr_des[i])
+lc_data.setContactState(contact_state)
+
+#fsm_data = ControlFSMData()
+#print(fsm_data)
+fsm = ControlFSM(model, StateEstimatorContainer(), LegController(), GaitScheduler(), DesiredStateCommand(), RobotControlParameters(), VisualizationData(), MIT_UserParameters())
+
+#lc.run(lc_data, fsm_data)
 
 # RobotRunner
-'''
+
 ctrl = MIT_Controller()
 task_manager = PeriodicTaskManager()
 
-runner = RobotRunner(ctrl, task_manager, 0, "robot-task")
-user_params = ctrl.getUserControlParameters()
+#ctrl.initializeController()
+#ctrl.runController()
 
-runner.init()
-runner.run()
-'''
+runner = RobotRunner(ctrl, task_manager, 0, "robot-task")
+
+print("ok")
+
+orientation = np.array([1.0, 0.0, 0.0, 0.0])
+position = np.array([0.0, 0.0, 0.29])
+omegaBody = np.array([0.0, 0.0, 0.0])
+vBody = np.array([0.0, 0.0, 0.0])
+acceleration = np.array([0.0, 0.0, 0.0])
+
+runner.setupData(orientation, position, omegaBody, vBody, acceleration);
+print("setup complete!")
+
+
+#runner.run(:q:q
+ctrl.runController()
+print("one run!")
+ctrl.runController()
+print("two runs!")
+
+
+#user_params = ctrl.getUserControlParameters()
+
+#runner.init()
+#runner.run()
+
--- python/bindings.cpp
+++ python/bindings.cpp
@@ -28,6 +28,16 @@ namespace py = pybind11;
 #include "Controllers/StateEstimatorContainer.h"
 #include "Controllers/DummyStateEstimatorContainer.h"
 
+#include "ControlParameters/ControlParameterInterface.h"
+#include "SimUtilities/GamepadCommand.h"
+#include "SimUtilities/IMUTypes.h"
+#include "SimUtilities/SpineBoard.h"
+#include "SimUtilities/VisualizationData.h"
+#include "SimUtilities/ti_boardcontrol.h"
+#include "Utilities/SharedMemory.h"
+
+
+
 
 PYBIND11_MODULE(pycheetah, m) {
 
@@ -49,6 +59,22 @@ PYBIND11_MODULE(pycheetah, m) {
 
         py::class_<LocomotionCtrlData<float>>(m, "LocomotionCtrlData")
 		.def(py::init<>())
+		.def("setBodyDes", [](LocomotionCtrlData<float> &self, Vec3<float> pBody_des, Vec3<float> vBody_des, Vec3<float> aBody_des, Vec3<float> pBody_RPY_des, Vec3<float> vBody_Ori_des){
+			self.pBody_des = pBody_des;
+			self.vBody_des = vBody_des;
+			self.aBody_des = aBody_des;
+			self.pBody_RPY_des = pBody_RPY_des;
+			self.vBody_Ori_des = vBody_Ori_des;
+			})
+		.def("setFootDes", [](LocomotionCtrlData<float> &self, int idx, Vec3<float> pFoot_des, Vec3<float> vFoot_des, Vec3<float> aFoot_des, Vec3<float> Fr_des){
+			self.pFoot_des[idx] = pFoot_des;
+			self.vFoot_des[idx] = vFoot_des;
+			self.aFoot_des[idx] = aFoot_des;
+			self.Fr_des[idx] = Fr_des;
+			})
+		.def("setContactState", [](LocomotionCtrlData<float> &self, Vec4<float> contact_state){
+			self.contact_state = contact_state;
+			})
 	        .def_readwrite("pBody_des", &LocomotionCtrlData<float>::pBody_des)
 		.def_readwrite("vBody_des", &LocomotionCtrlData<float>::vBody_des)
 		.def_readwrite("aBody_des", &LocomotionCtrlData<float>::aBody_des)
@@ -68,6 +94,18 @@ PYBIND11_MODULE(pycheetah, m) {
         py::class_<ControlFSMData<float>> fsmdata(m, "ControlFSMData");
 	fsmdata.def(py::init<>(), "Initialize the Control FSM Data");
 
+	py::class_<ControlFSM<float>> controlFSM(m, "ControlFSM");
+	controlFSM.def(py::init<Quadruped<float>*, StateEstimatorContainer<float>*, LegController<float>*, GaitScheduler<float>*, DesiredStateCommand<float>*, RobotControlParameters*, VisualizationData*, MIT_UserParameters*>());
+	controlFSM.def_readonly("data", &ControlFSM<float>::data);
+//			self._quadruped = _quadruped; 
+//			self._stateEstimator = _stateEstimator;
+//			self._legController = _legController;
+//			self._desiredStateCommand = _desiredStateCommand;
+//			self.controlParameters = controlParameters;
+//			self.userParameters = userParameters;
+//			self.visualizationData = visualizationData;
+//			})
+
 
         py::class_<RobotController> robotctrl(m, "RobotController");
 
@@ -84,15 +122,74 @@ PYBIND11_MODULE(pycheetah, m) {
 	robotrunner.def(py::init<RobotController*, PeriodicTaskManager*, float, std::string>());
         robotrunner.def("init", py::overload_cast<>(&RobotRunner::init), "");	
         robotrunner.def("run", py::overload_cast<>(&RobotRunner::run), "");
+        robotrunner.def("setupData", [](RobotRunner &self, Quat<double> orientation, Vec3<double> position,
+						Vec3<double> omegaBody, Vec3<double> vBody, Vec3<double> acceleration) {
+			//SimulatorToRobotMessage simToRobot;
+			std::cout << orientation;
+			GamepadCommand gamepadCommand = GamepadCommand();
+			SpiData spiData;
+			TiBoardData tiBoardData;
+			//RobotType robotType;
+			VectorNavData vectorNavData;
+			CheaterState<double> cheaterState;
+			cheaterState.orientation = orientation;
+			cheaterState.position = position;
+			cheaterState.omegaBody = omegaBody;
+			cheaterState.vBody = vBody;
+			cheaterState.acceleration = acceleration;
+			SpiCommand spiCommand;
+			TiBoardCommand tiBoardCommand;
+			RobotControlParameters controlParameters;
+			VisualizationData visualizationData;
+			CheetahVisualization cheetahVisualization;
+
+			std::cout << orientation;
+
+	       	        self.driverCommand = &gamepadCommand;
+			self.spiData = &spiData;
+	 		self.tiBoardData = &tiBoardData;
+			self.robotType = RobotType::MINI_CHEETAH;
+			self.vectorNavData = &vectorNavData;
+			self.cheaterState = &cheaterState;
+			self.spiCommand = &spiCommand;
+			self.tiBoardCommand = &tiBoardCommand;
+			self.controlParameters = &controlParameters;
+			self.visualizationData = &visualizationData;
+			self.cheetahMainVisualization = &cheetahVisualization;
+			
+			self.init();
+			});		
 
 	py::class_<StateEstimate<float>> se(m, "StateEstimate");
 
 	
-	//py::class_<StateEstimatorContainer<float>> secontainer(m, "StateEstimatorContainer");
+	py::class_<StateEstimatorContainer<float>> secontainer(m, "StateEstimatorContainer");
 	//secontainer.def(py::init<CheaterState<double>*, VectorNavData*. LegControllerData<float>*. StateEstimate<float>*, RobotControlParameters*>(), "");
 	//secontainer.def_readwrite("_data", _data);	
 	
-	py::class_<DummyStateEstimatorContainer<float>> dsecontainer(m, "DummyStateEstimatorContainer");
-	dsecontainer.def(py::init<StateEstimate<float>>(), "");, 
+	//py::class_<DummyStateEstimatorContainer<float>> dsecontainer(m, "DummyStateEstimatorContainer");
+	//dsecontainer.def(py::init<StateEstimate<float>>(), "");, 
+
+	// Simulator to robot messages
+	py::class_<GamepadCommand> gamepadcmd(m, "GamepadCommand");
+	gamepadcmd.def(py::init<>(), "");
+	
+	py::class_<RobotType> robottype(m, "RobotType");
+
+	py::class_<VectorNavData> vnavdata(m, "VectorNavData");
+	
+	py::class_<CheaterState<double>> cheaterstate(m, "CheaterState");
+	
+	py::class_<SpiData> spidata(m, "SpiData");
+	
+	py::class_<TiBoardData>tidata(m, "TiBoardData");
+	
+	py::class_<ControlParameterRequest> cpreq(m, "ControlParameterRequest");
+	
+	py::class_<RobotControlParameters> robotctrlparams(m, "RobotControlParameters");
+	
+	py::class_<VisualizationData> visdata(m, "VisualizationData");
+	
+
 
 }

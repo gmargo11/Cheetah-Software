de0ec682740dc93e7e45fdde32109924be9095ff
Gabriel Margolis <gmargo@mit.edu>
2020-06-12 21:48:57 +0000

    first iteration of raisim interface

--- python/raisim_control.py
+++ python/raisim_control.py
@@ -4,6 +4,8 @@ import time
 import pycheetah
 from pycheetah import * #FloatingBaseModel, ControlFSMData, LocomotionCtrl, LocomotionCtrlData, PeriodicTaskManager, RobotRunner, MIT_Controller
 
+import raisimpy as raisim
+
 #fb = FloatingBaseModel()
 #fb.addGroundContactPoint(0, np.array([1., 1., 1.]), False)
 #fb.addBase(1.0, np.ones(3), np.eye(3))
@@ -123,91 +125,152 @@ class Cheetah:
         return tauff, forceff, qDes, qdDes, pDes, vDes, kpCartesian, kdCartesian, kpJoint, kdJoint
 
 
+class CheetahSimulator:
+    def __init__(self):
+        self.setup_raisim_env()
+
+    def setup_raisim_env(self):
+        world = raisim.World()
+        world.set_time_step(0.0025)
+
+        self.vis = raisim.OgreVis.get()
+
+        # these methods must be called before initApp
+        self.vis.set_world(world)
+        self.vis.set_window_size(1800, 1200)
+        self.vis.set_default_callbacks()
+        self.vis.set_setup_callback(self.setup_callback)
+        self.vis.set_anti_aliasing(2)
+
+        # starts self.visualizer thread
+        self.vis.init_app()
+
+        # create raisim objects
+        ground = world.add_ground()
+        ground.set_name("checkerboard")
+
+        # create self.visualizer objects
+        self.vis.create_graphical_object(ground, dimension=20, name="floor", material="checkerboard_green")
+
+        N = 1
+        cheetah_urdf_path = os.path.dirname(os.path.abspath(__file__)) + "/./urdf/mini_cheetah_simple.urdf"
+
+        self.cheetah = world.add_articulated_system(cheetah_urdf_path)
+        print("dof:", self.cheetah.get_dof())
+
+        self.vis.create_graphical_object(self.cheetah, name="cheetah1")
+
+        # set camera 
+        camera = self.vis.get_camera_man().get_camera()
+        camera.set_position(N, -2 - N, 1.5 + N)
+        camera.pitch(1.)
+
+    def setup_callback(self):
+        self.vis = raisim.OgreVis.get()
+
+        # light
+        light = self.vis.get_light()
+        light.set_diffuse_color(1, 1, 1)
+        light.set_cast_shadows(True)
+        light.set_direction(self.normalize([-3., -3., -0.5]))
+        self.vis.set_camera_speed(300)
+
+        # load textures
+        self.vis.add_resource_directory(self.vis.get_resource_dir() + "/material/checkerboard")
+        self.vis.load_material("checkerboard.material")
+
+        # shadow setting
+        manager = self.vis.get_scene_manager()
+        manager.set_shadow_technique(raisim.ogre.ShadowTechnique.SHADOWTYPE_TEXTURE_ADDITIVE)
+        manager.set_shadow_texture_settings(2048, 3)
+
+        # scale related settings!! Please adapt it depending on your map size
+        # beyond this distance, shadow disappears
+        manager.set_shadow_far_distance(10)
+        # size of contact points and contact forces
+        self.vis.set_contact_visual_object_size(0.03, 0.6)
+        # speed of camera motion in freelook mode
+        self.vis.get_camera_man().set_top_speed(5)
+
+    def run_control(self, controller):
+        self.vis.set_control_callback(controller)
+	
+	# run control loop and record video
+        self.vis.start_recording_video("./raisim.mp4")
+        self.vis.run()
+        self.vis.stop_recording_video_and_save()
+        self.vis.close_app()
+
 #######################
-# Run Controller
+# Setup RaiSim Simulator
 #######################
 
 
-# initialization
-
-dt = 0.025
-cheetah_ctrl = Cheetah( robot_filename = "../config/mini-cheetah-defaults.yaml",
-                        user_filename = "../config/mc-mit-ctrl-user-parameters.yaml",
-                        dt = dt )
-
-# initialize controllers
-
-cmpc = ConvexMPCLocomotion(dt, (int)(30 / (1000 * dt)), cheetah_ctrl.userparams)
-wbc = LocomotionCtrl(cheetah_ctrl.model)
-wbc_data = LocomotionCtrlData()
-
-# control loop!
-tstart = time.time()
-
-iterations = 1000
-
-for i in range(iterations):
-
-    # update joint state
-    q = np.zeros(12) 
-    qd = np.zeros(12)
-    cheetah_ctrl.set_joint_state(q, qd)
-
-    # update cheater state
-    state = np.zeros(16) # [orientation(4), pos(3), omegaBody(3), vBody(3), accel(3)]
-    cheetah_ctrl.set_cartesian_state(state)
 
 
-    # run MPC
-    cmpc.run(cheetah_ctrl.fsm.data)
+#######################
+# Controller
+#######################
+
+class WBC_MPC_Controller:
+	# initialization
+    def __init__(self, cheetah_sim):
+        self.dt = 0.025
+        self.cheetah_ctrl = Cheetah( robot_filename = "../config/mini-cheetah-defaults.yaml",
+                                     user_filename = "../config/mc-mit-ctrl-user-parameters.yaml",
+                                     dt = dt )
+        self.cheetah_sim = cheetah_sim
+
+	# initialize controllers
+
+        self.cmpc = ConvexMPCLocomotion(dt, (int)(30 / (1000 * dt)), cheetah_ctrl.userparams)
+        self.wbc = LocomotionCtrl(cheetah_ctrl.model)
+        self.wbc_data = LocomotionCtrlData()
+
+    def __call__(self):
+        # update joint state
+        q, qd = self.cheetah_sim.get_states()
+        q = np.zeros(12) 
+        qd = np.zeros(12)
+        self.cheetah_ctrl.set_joint_state(q, qd)
+
+        # update cheater state
+        base_orientation = self.cheetah_ctrl.get_world_quaternion(0)
+        base_pos = self.cheetah_ctrl.get_world_position(0)
+        base_omega = self.cheetah_ctrl.get_world_angular_velocity(0)
+        base_vel = self.cheetah_ctrl.get_world_linear_velocity(0)
+        base_accel = self.cheetah_ctrl.get_generalized_forces()[0:3] / self.cheetah_ctrl.get_masses()[0]
+        state = np.concatenate((base_orientation, base_pos, base_omega, base_vel, base_accel)) # [orientation(4), pos(3), omegaBody(3), vBody(3), accel(3)]
+        self.cheetah_ctrl.set_cartesian_state(state)
+
+
+        # run MPC
+        self.cmpc.run(self.cheetah_ctrl.fsm.data)
 
     
-    wbc_data.setBodyDes(cmpc.pBody_des, cmpc.vBody_des, cmpc.aBody_des, cmpc.pBody_RPY_des, cmpc.vBody_Ori_des)
-    for i in range(4):
-        wbc_data.setFootDes(i, cmpc.get_pFoot_des(i), cmpc.get_vFoot_des(i), cmpc.get_aFoot_des(i), cmpc.get_Fr_des(i))
-    wbc_data.setContactState(cmpc.contact_state)
+        self.wbc_data.setBodyDes(self.cmpc.pBody_des, self.cmpc.vBody_des, self.cmpc.aBody_des, self.cmpc.pBody_RPY_des, self.cmpc.vBody_Ori_des)
+        for i in range(4):
+           self.wbc_data.setFootDes(i, self.cmpc.get_pFoot_des(i), self.cmpc.get_vFoot_des(i), self.cmpc.get_aFoot_des(i), self.cmpc.get_Fr_des(i))
+        self.wbc_data.setContactState(self.cmpc.contact_state)
 
-    # run WBC
-    wbc.run(wbc_data, cheetah_ctrl.fsm.data)
+        # run WBC
+        self.wbc.run(self.wbc_data, self.cheetah_ctrl.fsm.data)
 
-    # get control output
-    tauff, forceff, qDes, qdDes, pDes, vDes, kpCartesian, kdCartesian, kpJoint, kdJoint = cheetah_ctrl.get_joint_commands()
-    print(tauff, forceff, qDes, qdDes)
+        # get control output
+        tauff, forceff, qDes, qdDes, pDes, vDes, kpCartesian, kdCartesian, kpJoint, kdJoint = self.cheetah_ctrl.get_joint_commands()
+        print(tauff, forceff, qDes, qdDes)
 
-print("control frequency: ", iterations/(time.time()-tstart))
-print("required frequency: ", 1/dt)
-
-# RobotRunner
-'''
-ctrl = MIT_Controller()
-task_manager = PeriodicTaskManager()
-
-#ctrl.initializeController()
-#ctrl.runController()
-
-runner = RobotRunner(ctrl, task_manager, 0, "robot-task")
-
-print("ok")
-
-orientation = np.array([1.0, 0.0, 0.0, 0.0])
-position = np.array([0.0, 0.0, 0.29])
-omegaBody = np.array([0.0, 0.0, 0.0])
-vBody = np.array([0.0, 0.0, 0.0])
-acceleration = np.array([0.0, 0.0, 0.0])
-
-runner.setupData(orientation, position, omegaBody, vBody, acceleration);
-print("setup complete!")
+        self.cheetah_ctrl.set_pd_targets(np.pad(qDes, (7, 0)), np.pad(qdDes, (6, 0)))
+        self.cheetah_ctrl.set_pd_gains(np.pad(kpJoint, (6, 0)), np.pad(kdJoint, (6, 0)))
 
 
-#runner.run(:q:q
-ctrl.runController()
-print("one run!")
-ctrl.runController()
-print("two runs!")
 
+##########################
+# Run everything
+##########################
 
-#user_params = ctrl.getUserControlParameters()
+simulator = CheetahSimulator()
+controller = WBC_MPC_Controller(simulator.cheetah)
 
-#runner.init()
-#runner.run()
-'''
+simulator.run_control(controller)
+
